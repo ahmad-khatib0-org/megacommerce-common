85705905d826fef432e397798cf93f11604c4fd3
ahmad-khatib0 <ahmadkhatib923@gmail.com>
2025-07-21 15:50:00 +0300

    create tokens table

---
+++ migrations/000012_create_tokens_table.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS tokens (
+  id VARCHAR(26) PRIMARY KEY,
+  token VARCHAR(256) NOT NULL,
+  type VARCHAR(64) NOT NULL,
+  created_at BIGINT NOT NULL,
+  expires_at BIGINT NOT NULL
+);
