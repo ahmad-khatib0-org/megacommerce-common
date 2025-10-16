a3f5c7c1cc44dfebc86426dad63416d79fde435a
ahmad-khatib0 <ahmadkhatib923@gmail.com>
2025-10-16 13:55:31 +0300

    add translations , image to categories table, add more translations

--- migrations/000005_create_categories_table.up.sql
+++ migrations/000005_create_categories_table.up.sql
@@ -1,7 +1,9 @@
 CREATE TABLE IF NOT EXISTS categories (
   id VARCHAR(64) PRIMARY KEY,
   name VARCHAR(64) NOT NULL,
+  image VARCHAR(256) NOT NULL,
   subcategories JSONB NOT NULL,
+  translations JSONB NOT NULL,
   edits JSONB NOT NULL,
   version SMALLINT NOT NULL,
   created_at TIMESTAMP NOT NULL,
